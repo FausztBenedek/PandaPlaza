8ef447c2218ceef36209f58983ee5c2c655be9d0
FausztBenedek <fausztb@email.com>
2019-03-13 19:56:25 +0100

    ForgatokonyvListaElem interfész hozzáadva

---
+++ src/ForgatokonyvListaElem.java
@@ -0,0 +1,18 @@
+
+/** A forgatókönyvek listájának elemei implementálják.
+ */
+public interface ForgatokonyvListaElem {
+	
+	/**
+	 * Leírás a forgatókönyvhöz.
+	 * @return - A forgatókönyv leírása.
+	 */
+	public String getLeiras();
+	
+	/**
+	 * A forgatókönyvhöz tartozó objektumok példányosításához, 
+	 * a forgatókönyv inicializálásához, és az inditásnak megfelelő
+	 * metódus meghívásához szükséges logika.
+	 */
+	public void action();
+}
