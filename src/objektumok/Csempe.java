389f8d90897b1b1237550ca50a74e289889bfec2
Fehérvári Attila <djindic@gmail.com>
2019-03-20 22:50:16 +0100

    csempe osztály törzse implementálva
    A Játékgép, Fotel, Csokiautómata osztályok használják a getneighbours függvényt ami már nincs. Helyette getRandomSzomszed van

--- src/objektumok/Csempe.java
+++ src/objektumok/Csempe.java
@@ -1,34 +1,71 @@
 package objektumok;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /** */
 public class Csempe {
-	/** */
+	
+	//attribútumok
+	private Dolog dolog;
+	private ArrayList<Csempe> szomszedok;
+	
+	//Függvények
+	
+	/** 
+	 * Visszaadja azt a csempét ami a paraméterül kapott irányban van
+	 * 
+	 * */
 	public Csempe getNeigbour(int irany) {
-		return null; // TODO
+		return szomszedok.get(irany);
 	}
 	
-	/** */
+	/** 
+	 *Beállítja a csempe dolog attribútumát a paraméterül kapott állatra illetve az állat csempe
+	 *attribútumát saját magára
+	 * */
 	public void accept(Allat a) {
+		this.setDolog(a);
+		a.setCsempe(this);
 	}
 	
-	/** */
+	/** 
+	 * Visszaadja a csempén tartózkodó dolgot
+	 * */
 	public Dolog getDolog() {
-		return null; // TODO
+	
+		return dolog; // TODO
 	}
 	
-	/** */
+	/**
+	 * Beállítja a paraméterül kapott dolgot a csempére
+	 *  */
 	public void setDolog(Dolog d) {
+		dolog = d;
 	}
 	
-	/** */
+	/**
+	 * A paraméterül kapott csempét beállítja a saját adott irányban található szomszédjaként
+	 *  */
 	public void setNeighbour(int irany, Csempe c) {
+		szomszedok.set(irany, c);
 	}
 	
-	/** */
+	/**
+	 * Lekezeli azt hogy egy rajta álló panda ugrott. A normál csempe esetén nem történik semmi
+	 *  */
 	public void ugrott() {
 	}
 	
-	public int[] getIranyok() {
-		return null; // TODO
+	/**
+	 * Visszaad egy random szomszédos csempét
+	 * @return
+	 */
+	public Csempe getRandomSzomszed() {
+		Random rand = new Random();
+		int irany = rand.nextInt(szomszedok.size()); // elvileg ez a felső határt kapja paraméterül (az alsó 0) és annál szigorúan kisebbeket generál
+		return szomszedok.get(irany);	
+		
+		
 	}
 }
