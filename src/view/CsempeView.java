2bda955307a17b1214eea8e40286bd4b33315136
FausztBenedek <fausztb@email.com>
2019-05-09 15:33:44 +0200

    CsempeView osztály javítása, interfészének kibővítése. isInside(), color

--- src/view/CsempeView.java
+++ src/view/CsempeView.java
@@ -16,22 +16,22 @@ public class CsempeView implements Drawable {
     /**
      * A modellben lévő csempe.
      */
-    Csempe represented;
+    private Csempe represented;
     
     /**
-     * A csempék átmérője.
+     * A csempe színe.
+     */
+    private Color color = Color.WHITE;
+    
+    /**
+     * A csempék sugara.
      */
     private static final int rad = 20;
-    
-    /**
-     * A csempe kék színe
-     */
-    private static final Color csempeBlue = new Color(15,15,255);
-    
+        
     /**
      * A csempe középpontjának a koordinátái.
      */
-    int x, y;
+    private int x, y;
     
     /**
      * Konstruktor. 
@@ -52,9 +52,55 @@ public class CsempeView implements Drawable {
     @Override
     public void draw() {
         Graphics g = DrawPanel.getG();
-        g.setColor(Color.RED);
+        g.setColor(color);
         int xBalFelso = x - rad;
         int yBalFelso = y - rad;
         g.fillOval(xBalFelso, yBalFelso, 2 * rad, 2 * rad);
+    }
+
+    /**
+     * A modellben lévő csempére getter.
+     * @return 
+     */
+    public Csempe getRepresented() {
+        return represented;
+    }
+
+    /**
+     * Annak a meghatározására, hogy a csempén belül van-e
+     * a paraméterként átadott pont.
+     * @param x A pont x koordinátája
+     * @param y A pont y koordinátája
+     * @return True, ha a pont a csempében van, false, ha nem.
+     */
+    public boolean isInside(int x, int y) {
+        // távolság = sqrt( (x-cx)^2 + (y-cy)^2 )
+        // Vagyis a középpontból a paraméterként átadott pontba mutató vektor hossza
+        // c a center, itt most cx = this.x, cy = this.y
+        
+        double xDistance = Math.abs(x - this.x);
+        double yDistance = Math.abs(y - this.y);
+        
+        double distanceFromCenter = Math.sqrt(
+                xDistance * xDistance + yDistance * yDistance);
+        
+        // Ha ez a távolság kisebb, mint a sugár => benne van a pont a körben.
+        return rad >= xDistance;
+    }
+    
+    /**
+     * A csempe színe.
+     * @return A csempe aktuális színe.
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /**
+     * Beállítja a csempe színét.
+     * @param color Az új szín
+     */
+    public void setColor(Color color) {
+        this.color = color;
     }
 }
