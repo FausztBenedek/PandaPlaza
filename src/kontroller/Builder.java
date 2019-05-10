2703c5418bb5e2ed6616fa6e9278d2bda1ae3e84
FausztBenedek <fausztb@email.com>
2019-05-10 15:22:12 +0200

    Objektum inicializálások kiszervezése

---
+++ src/kontroller/Builder.java
@@ -0,0 +1,83 @@
+
+package kontroller;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import objektumok.*;
+import view.*;
+
+/**
+ * Olyan statikus függvények gyűjteménye, amelyek a játékhoz szükséges objektumokat
+ * készítik el, és inicializálják. (pl.: hozzákötik a View-hez vagy betültenek
+ * egy képet stb)
+ */
+public class Builder {
+    
+    /**
+     * Eventeket dobáló objektum, amikor az egér belép egy csempére, vagy
+     * kilép egy csempéről. Azért van itt tárolva, mert egy két objektumhoz
+     * tartozó View-nak fel kell ezekre az eseményekre iratkoznia.
+     */
+    private static MouseBeKiPoller mouseBeKiEvent = new MouseBeKiPoller();
+    
+    /**
+     * Az orángutánok képe.
+     */
+    private static BufferedImage orangutanImage = null;
+    
+    /**
+     * Elkészít és inicializál egy csempét.
+     * @param x A csempét megjelenítő kör x koordinátája.
+     * @param y A csempét megjelenítő kör y koordinátája.
+     * @return Az elkészített csempe.
+     */
+    public static Csempe createCsempe(int x, int y) {
+        Csempe c = new Csempe();
+        CsempeView cView = new CsempeView(c, x, y);
+        c.setView(cView);
+        mouseBeKiEvent.addCsempeView(cView);
+        View.getInstance().add(cView);
+        return c;
+    }
+    
+    /**
+     * Elkészít és inicializál egy orángutánt.
+     * @param startPos Az orángután kezdő csempéje.
+     * @return Az elkészített orángután.
+     * @throws IllegalArgumentException Ha az orángutánt olyan csempére akarjuk
+     * inicializálni, ahol már van egy dolog.
+     */
+    public static Orangutan createOrangutan(Csempe startPos) throws IllegalArgumentException {
+        if (startPos.getDolog() != null) {
+            throw new IllegalArgumentException("A csempén már van egy dolog.");
+        }
+        Orangutan o = new Orangutan();
+        o.setCsempe(startPos);
+        startPos.setDolog(o);
+        // Kép betöltése, ha még nincs betöltve.
+        if (orangutanImage == null) {
+            try {
+                orangutanImage = ImageIO.read(new File(ImagePaths.orangutan));
+            } catch (IOException ex) {
+                Logger.getLogger(Initializer.class.getName()).log(Level.SEVERE, null, ex);
+                System.exit(-1);
+            }
+        }
+        OrangutanView oVeiw = new OrangutanView(o, orangutanImage);
+        View.getInstance().add(oVeiw);
+        return o;
+    }
+    
+    /**
+     * Eventeket dobáló objektum, amikor az egér belép egy csempére, vagy
+     * kilép egy csempéről. Azért van itt tárolva, mert egy két objektumhoz
+     * tartozó View-nak fel kell ezekre az eseményekre iratkoznia.
+     */
+    public static MouseBeKiPoller getMouseBeKiEvent() {
+        return mouseBeKiEvent;
+    }
+}
--- src/kontroller/Initializer.java
+++ src/kontroller/Initializer.java
@@ -2,13 +2,6 @@
 package kontroller;
 
 import GUI.DrawPanel;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
-import view.*;
 import objektumok.*;
 
 /**
@@ -16,25 +9,22 @@ import objektumok.*;
  */
 public class Initializer {
     
-    public static MouseBeKiPoller mouseBeKiEvent = new MouseBeKiPoller();
-    public static BufferedImage orangutanImage = null;
-
     /**
      * Inicializálja a pályát.
      */
     public static void initialize() {
         DrawPanel drawPanel = DrawPanel.getInstance();
         // Eseménydobálók inicializálása
-        drawPanel.addMouseMotionListener(mouseBeKiEvent);
+        drawPanel.addMouseMotionListener(Builder.getMouseBeKiEvent());
         // Orángutánt léptető logika inicializálása
         OrangutanLepteto orangutanLepteto = new OrangutanLepteto();
-        drawPanel.addMouseListener(orangutanLepteto);
-        mouseBeKiEvent.subscribe(orangutanLepteto);
+        drawPanel.addMouseListener(orangutanLepteto); // Kattintás detektálásához
+        Builder.getMouseBeKiEvent().subscribe(orangutanLepteto); // Csempékbe való be-, kilépések detektálásához
         
         // 3x3-as négyzet sorfojtonosan
-        Csempe c1 = createCsempe(100,100); Csempe c2 = createCsempe(200,100); Csempe c3 = createCsempe(300,100);
-        Csempe c4 = createCsempe(100,200); Csempe c5 = createCsempe(200,200); Csempe c6 = createCsempe(300,200);        
-        Csempe c7 = createCsempe(100,300); Csempe c8 = createCsempe(200,300); Csempe c9 = createCsempe(300,300);
+        Csempe c1 = Builder.createCsempe(100,100); Csempe c2 = Builder.createCsempe(200,100); Csempe c3 = Builder.createCsempe(300,100);
+        Csempe c4 = Builder.createCsempe(100,200); Csempe c5 = Builder.createCsempe(200,200); Csempe c6 = Builder.createCsempe(300,200);        
+        Csempe c7 = Builder.createCsempe(100,300); Csempe c8 = Builder.createCsempe(200,300); Csempe c9 = Builder.createCsempe(300,300);
         
         // Vízszintes kapcsolatok
         osszekot(1, c1, c2);
@@ -51,43 +41,13 @@ public class Initializer {
         osszekot(11, c3, c6);
         osszekot(12, c6, c9);
         
-        Orangutan o = createOrangutan(c5);
-        Orangutan o2 = createOrangutan(c9);
+        Orangutan o = Builder.createOrangutan(c5);
+        Orangutan o2 = Builder.createOrangutan(c9);
         Game.getInstance().setActiveOrangutan(o);
     }
     
     public static void osszekot(int id, Csempe c1, Csempe c2) {
         c1.setNeighbour(id, c2);
         c2.setNeighbour(id, c1);
-    }
-    
-    public static Csempe createCsempe(int x, int y) {
-        Csempe c = new Csempe();
-        CsempeView cView = new CsempeView(c, x, y);
-        c.setView(cView);
-        mouseBeKiEvent.addCsempeView(cView);
-        View.getInstance().add(cView);
-        return c;
-    }
-    
-    public static Orangutan createOrangutan(Csempe startPos) {
-        if (startPos.getDolog() != null) {
-            throw new IllegalArgumentException("A csempén már van egy dolog.");
-        }
-        Orangutan o = new Orangutan();
-        o.setCsempe(startPos);
-        startPos.setDolog(o);
-        // Kép betöltése, ha még nincs betöltve.
-        if (orangutanImage == null) {
-            try {
-                orangutanImage = ImageIO.read(new File(ImagePaths.orangutan));
-            } catch (IOException ex) {
-                Logger.getLogger(Initializer.class.getName()).log(Level.SEVERE, null, ex);
-                System.exit(-1);
-            }
-        }
-        OrangutanView oVeiw = new OrangutanView(o, orangutanImage);
-        View.getInstance().add(oVeiw);
-        return o;
     }
 }
